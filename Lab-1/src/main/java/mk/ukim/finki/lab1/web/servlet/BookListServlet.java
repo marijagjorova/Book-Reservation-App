73c3a795dc9495e6f7911167e3cbcf0ea187be5e
unknown <marija.gjorova@students.finki.ukim.mk>
2025-10-31 15:40:02 +0100

    Plus questions

--- Lab-1/src/main/java/mk/ukim/finki/lab1/web/servlet/BookListServlet.java
+++ Lab-1/src/main/java/mk/ukim/finki/lab1/web/servlet/BookListServlet.java
@@ -11,6 +11,8 @@ import org.thymeleaf.web.IWebExchange;
 import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 @WebServlet(name = "BookListServlet", urlPatterns = "")
@@ -42,15 +44,41 @@ public class BookListServlet extends HttpServlet {
 
         }
 
-        List<Book> books;
-        if ((filterName == null || filterName.isEmpty()) && filterRatingStr == null) {
-            books = this.bookService.listAll();
-        } else {
-            books = this.bookService.searchBooks(filterName, filterRating);
+        List<Book> books = bookService.listAll();
+
+        HashMap<String, Integer> bookReservationCount = (HashMap<String, Integer>) getServletContext().getAttribute("bookReservationCount");
+
+        Book mostPopularBook = null;
+        int maxReservations = -1;
+
+        for (Book book: books){
+            int counter = bookReservationCount.getOrDefault(book.getTitle(), 0);
+            if (counter > maxReservations){
+                maxReservations = counter;
+                mostPopularBook= book;
+            }
         }
 
+
+        List<Book> bookss;
+        if ((filterName == null || filterName.isEmpty()) && filterRatingStr == null) {
+            bookss = this.bookService.listAll();
+        } else {
+            bookss = this.bookService.searchBooks(filterName, filterRating);
+        }
+
+
         WebContext context = new WebContext(webExchange);
-        context.setVariable("books", books);
+
+
+        if (mostPopularBook !=null && maxReservations >0) {
+            context.setVariable("mostPopularBook", mostPopularBook);
+            context.setVariable("mostPopularBookCount", maxReservations);
+        }
+        else{
+            context.setVariable("mostPopularBookMess", "No reserved books");
+        }
+        context.setVariable("books", bookss);
 
         templateEngine.process("listBooks", context, response.getWriter());
     }
@@ -59,6 +87,14 @@ public class BookListServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String filterName = request.getParameter("filterName");
         String filterRating = request.getParameter("filterRating");
+        String bookIdStr = request.getParameter("bookId");
+        if (bookIdStr != null){
+            long bookId = Long.parseLong(bookIdStr);
+
+            HashMap<Long, Integer> bookReservationCount = (HashMap<Long, Integer>) getServletContext().getAttribute("bookReservationCount");
+            bookReservationCount.put(bookId, bookReservationCount.getOrDefault(bookId, 0) +1);
+        }
+        response.sendRedirect("/");
 
         String params = String.format("filterName=%s&filterRating=%s", filterName, filterRating);
         response.sendRedirect("/?" + params);
